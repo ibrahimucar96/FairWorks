d1e56c2bc141920e1ca15e49a58a22e1fe7becaf
ibrahim uçar <ibo_beko_furki@hotmail.com>
2022-06-17 17:40:32 +0300

    Admin Bölümüne _Layout eklendi

--- FairWorks.WebUI/Startup.cs
+++ FairWorks.WebUI/Startup.cs
@@ -90,6 +90,14 @@ namespace FairWorks.WebUI
 
             app.UseEndpoints(endpoints =>
             {
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+                });
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
