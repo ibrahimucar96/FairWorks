2ab03cbc7cbd93f86ad1b6322d880eb2ca890bd8
ibrahim uçar <ibo_beko_furki@hotmail.com>
2022-06-24 00:17:38 +0300

    PersonelController yazildi

---
+++ FairWorks.WebUI/Areas/Admin/Controllers/PersonelController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FairWorks.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FairWorks.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class PersonelController : Controller
+    {
+        private readonly FairWorksDbContext _context;
+
+        public PersonelController(FairWorksDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Personeller.ToListAsync());
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personel = await _context.Personeller
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (personel == null)
+            {
+                return NotFound();
+            }
+
+            return View(personel);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ad,SoyAd,TcNo,Email,Unvan,BirthDate,Adres,Sehir,Bolge,Ulke,Telefon,Fotograf,Notlar,RaporVerdigiKisi,PhotoPath,Maas")] Personel personel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(personel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personel);
+        }
+
+       
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personel = await _context.Personeller.FindAsync(id);
+            if (personel == null)
+            {
+                return NotFound();
+            }
+            return View(personel);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ad,SoyAd,TcNo,Email,Unvan,BirthDate,Adres,Sehir,Bolge,Ulke,Telefon,Fotograf,Notlar,RaporVerdigiKisi,PhotoPath,Maas")] Personel personel)
+        {
+            if (id != personel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(personel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonelExists(personel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personel);
+        }
+
+       
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var personel = await _context.Personeller
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (personel == null)
+            {
+                return NotFound();
+            }
+
+            return View(personel);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var personel = await _context.Personeller.FindAsync(id);
+            _context.Personeller.Remove(personel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonelExists(int id)
+        {
+            return _context.Personeller.Any(e => e.Id == id);
+        }
+    }
+}
