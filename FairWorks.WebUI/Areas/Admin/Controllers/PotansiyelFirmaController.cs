a6be233a5caea5806386fdde91c8c0958d0fe101
ibrahim uçar <ibo_beko_furki@hotmail.com>
2022-06-24 01:12:30 +0300

    PotansiyelFirma ve Salon Controller yazıldı

---
+++ FairWorks.WebUI/Areas/Admin/Controllers/PotansiyelFirmaController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FairWorks.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FairWorks.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class PotansiyelFirmaController : Controller
+    {
+        private readonly FairWorksDbContext _context;
+
+        public PotansiyelFirmaController(FairWorksDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PotansiyelFirmalar.ToListAsync());
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var potansiyelFirma = await _context.PotansiyelFirmalar
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (potansiyelFirma == null)
+            {
+                return NotFound();
+            }
+
+            return View(potansiyelFirma);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirmaAdı,Yetkili,Unvani,Telefon,DahiliTelefon,DirektTelefon,Email,Faks,Adres,Ulke,Sehir,Sektor")] PotansiyelFirma potansiyelFirma)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(potansiyelFirma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(potansiyelFirma);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var potansiyelFirma = await _context.PotansiyelFirmalar.FindAsync(id);
+            if (potansiyelFirma == null)
+            {
+                return NotFound();
+            }
+            return View(potansiyelFirma);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirmaAdı,Yetkili,Unvani,Telefon,DahiliTelefon,DirektTelefon,Email,Faks,Adres,Ulke,Sehir,Sektor")] PotansiyelFirma potansiyelFirma)
+        {
+            if (id != potansiyelFirma.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(potansiyelFirma);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PotansiyelFirmaExists(potansiyelFirma.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(potansiyelFirma);
+        }
+
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var potansiyelFirma = await _context.PotansiyelFirmalar
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (potansiyelFirma == null)
+            {
+                return NotFound();
+            }
+
+            return View(potansiyelFirma);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var potansiyelFirma = await _context.PotansiyelFirmalar.FindAsync(id);
+            _context.PotansiyelFirmalar.Remove(potansiyelFirma);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PotansiyelFirmaExists(int id)
+        {
+            return _context.PotansiyelFirmalar.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FairWorks.WebUI/Areas/Admin/Controllers/SalonController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FairWorks.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FairWorks.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class SalonController : Controller
+    {
+        private readonly FairWorksDbContext _context;
+
+        public SalonController(FairWorksDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Salonlar.ToListAsync());
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ulke,Sehir,SalonNo,m2,en,boy,SektorAdi")] Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar.FindAsync(id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+            return View(salon);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ulke,Sehir,SalonNo,m2,en,boy,SektorAdi")] Salon salon)
+        {
+            if (id != salon.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalonExists(salon.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salon = await _context.Salonlar.FindAsync(id);
+            _context.Salonlar.Remove(salon);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalonExists(int id)
+        {
+            return _context.Salonlar.Any(e => e.Id == id);
+        }
+    }
+}
